bffb311778f524777c34aafbe4da481a05a4b6fb
Alexander Urizar <urizaraj@gmail.com>
2018-03-05 18:18:29 -0500

    Fixed Typo

--- db/migrate/20180305231304_create_user_shows.rb
+++ db/migrate/20180305231304_create_user_shows.rb
@@ -2,7 +2,7 @@ class CreateUserShows < ActiveRecord::Migration[5.1]
   def change
     create_table :user_shows do |t|
       t.integer :user_id
-      t.ingeger :show_id
+      t.integer :show_id
     end
   end
 end
