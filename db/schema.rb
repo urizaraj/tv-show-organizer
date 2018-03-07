feb7ab51249a6d34f868f04f315f640bc2876d03
urizaraj <32577918+urizaraj@users.noreply.github.com>
2018-03-07 16:28:59 -0500

    Add scheme

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305210724) do
+ActiveRecord::Schema.define(version: 20180307175145) do
 
   create_table "shows", force: :cascade do |t|
     t.string "name"
+    t.integer "year"
+    t.string "description"
+  end
+
+  create_table "user_shows", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "show_id"
   end
 
   create_table "users", force: :cascade do |t|
