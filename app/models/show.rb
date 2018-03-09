02e2fa3bf74c4409c6542d5eb7943405d3ee4628
Alexander Urizar <urizaraj@gmail.com>
2018-03-09 15:20:42 -0500

    Add show validations

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,8 @@
 class Show < ActiveRecord::Base
   has_many :user_shows
   has_many :users, through: :user_shows
+
+  validates :name, presence: true
+  validates :year, presence: true,
+                   numericality: { only_integer: true, greater_than: 1940, less_than: 2300 }
 end
