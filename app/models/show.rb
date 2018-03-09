ed778307fb2bf0c6a84ee678f4b58ed72a786413
Alexander Urizar <urizaraj@gmail.com>
2018-03-09 15:27:05 -0500

    Formatting, use or equal to

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,11 @@ class Show < ActiveRecord::Base
   has_many :users, through: :user_shows
 
   validates :name, presence: true
-  validates :year, presence: true,
-                   numericality: { only_integer: true, greater_than: 1940, less_than: 2300 }
+  validates :year,
+            presence: true,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 1940,
+              less_than_or_equal_to: 2300
+            }
 end
